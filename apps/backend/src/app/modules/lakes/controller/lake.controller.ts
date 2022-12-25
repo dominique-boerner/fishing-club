35dfc510affdda4464ca8d1e71cf6c1778f65d46
Dominique Börner <dominique@mukosoft.de>
2022-12-25 22:27:11 +0100

    create crud for lakes

--- apps/backend/src/app/modules/lakes/controller/lake.controller.ts
+++ apps/backend/src/app/modules/lakes/controller/lake.controller.ts
@@ -1,24 +1,55 @@
-import { Controller, Get, Logger } from '@nestjs/common';
-import { LakeService } from '../services/lake.service';
+import { Body, Controller, Delete, Get, Post, Query } from "@nestjs/common";
+import { LakeService } from "../services/lake.service";
+import { Lake, LakeDocument } from "../schemas/lake.schema";
+import { ApiBody, ApiQuery } from "@nestjs/swagger";
 
 /**
  * LakeController
- * @description - A controller which handles CRUD operations
- *                for lakes.
+ *
+ * @description - A controller which defines REST-Endpoints for CRUD operations
+ *                of lakes.
+ * @see LakeService
  */
 @Controller('lake')
 export class LakeController {
-  constructor(private readonly lakeService: LakeService) {
+  constructor(private readonly lakeService: LakeService) {}
 
+  /**
+   * @description - Create a new lake and save it into the database.
+   * @param lake - Lake to create
+   */
+  @Post('')
+  @ApiBody({ type: Lake })
+  create(@Body() lake: LakeDocument) {
+    return this.lakeService.create(lake);
   }
 
+  /**
+   * @description - Get every lake from the database.
+   */
   @Get('')
   getAllLakes() {
     return this.lakeService.getAllLakes();
   }
 
+  /**
+   * @description - returns the amount of lakes from the database.
+   */
   @Get('/count')
   countAllLakes() {
     return this.lakeService.countLakes();
+  }
+
+  /**
+   * @description - remove a lake from the database.
+   * @param query - query parameters, should include the parameter "name"
+   */
+  @Delete('')
+  @ApiQuery({
+    name: 'name',
+    type: String,
+  })
+  delete(@Query() query: { name: string }) {
+    return this.lakeService.delete(query.name);
   }
 }
--- apps/backend/src/app/modules/lakes/services/lake.service.ts
+++ apps/backend/src/app/modules/lakes/services/lake.service.ts
@@ -1,17 +1,51 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Lake, LakeDocument } from '../schemas/lake.schema';
 import { Model } from 'mongoose';
 
+/**
+ * LakeService
+ *
+ * @description - This service handles CRUD operations for lakes.
+ */
 @Injectable()
 export class LakeService {
   constructor(@InjectModel(Lake.name) private lakeModel: Model<LakeDocument>) {}
 
+  /**
+   * @description - Create a new lake and save it into the database.
+   * @param lake - Lake to create
+   */
+  async create(lake: LakeDocument) {
+    Logger.log(`Create a new lake`, LakeService.name);
+    const model = new this.lakeModel({ ...lake });
+    await model.save();
+    return this.getAllLakes();
+  }
+
+  /**
+   * @description - Get every lake from the database.
+   */
   async getAllLakes() {
+    Logger.log(`Get all lakes`, LakeService.name);
     return this.lakeModel.find({});
   }
 
+  /**
+   * @description - returns the amount of lakes from the database.
+   */
   async countLakes() {
-    return this.lakeModel.countDocuments()
+    Logger.log(`Count all lakes`, LakeService.name);
+    return this.lakeModel.countDocuments();
+  }
+
+  /**
+   * @description - remove a lake from the database.
+   * @param name
+   */
+  async delete(name: string) {
+    Logger.log(`Delete lake with name ${name}`, LakeService.name);
+    await this.lakeModel.find({ name }).deleteMany();
+    return this.getAllLakes();
   }
 }
