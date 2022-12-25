19ba0fc7f17a745bae36605ee8185244e1945018
Dominique Börner <dominique@mukosoft.de>
2022-12-25 13:17:28 +0100

    install swagger for NestJS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,17 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder().setTitle('Fishing Club API').build();
+  const document = SwaggerModule.createDocument(app, config);
+
   const port = process.env.PORT || 3333;
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/api}`);
 }
 
 bootstrap();
